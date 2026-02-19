a4846331dcf626e3c721ed9bbdf9e5e89cd2cf8f
Eriyeza Buwembo <buwembo.eriyeza@gmail.com>
2026-02-18 18:21:32 -0600

    complete day 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import Cocoa
 
+// Day 1
 var greeting = "Hello, playground"
+let myName = "Eriyeza"
+
+print(greeting)
+greeting = "Yo"
+print(greeting)
+print(1.2 + 2.1)
+let num1 = 0.1
+let num2 = 0.2
+print(num1 + num2)
