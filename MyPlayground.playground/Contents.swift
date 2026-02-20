93b02319fceb345c4a9d0a4f0f30d67c60f97b96
Eriyeza Buwembo <buwembo.eriyeza@gmail.com>
2026-02-20 17:28:07 -0600

    finish day 2 and checkpoint 1

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Cocoa
+import Foundation
 
 // Day 1
 var greeting = "Hello, playground"
@@ -11,3 +12,24 @@ print(1.2 + 2.1)
 let num1 = 0.1
 let num2 = 0.2
 print(num1 + num2)
+
+// Day 2
+var jobFinished = false
+while !jobFinished {
+    print("Is job finished? I don't think so")
+    jobFinished.toggle()
+}
+print("Job finished")
+
+// Checkpoint 1
+let prompt = """
+    This program converts a temperature in Celsius to
+    Fahrenheit. 
+    """
+print(prompt)
+let celsius = 18.0
+print("You entered \(celsius) Celsius")
+let fahreneit = celsius * 1.8 + 32.0
+print("That is equal to \(fahreneit) Fahrenheit.")
+
+
