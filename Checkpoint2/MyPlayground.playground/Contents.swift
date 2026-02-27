149a834ee32a581aa2efd14efca8f3c8011e0cca
Eriyeza Buwembo <buwembo.eriyeza@gmail.com>
2026-02-27 15:25:48 -0600

    complete day 3 and checkpoint 2

---
+++ Checkpoint2/MyPlayground.playground/Contents.swift
@@ -0,0 +1,52 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var nums = [Int]()
+nums.append(1)
+nums.append(3)
+nums.append(2)
+nums.append(10)
+nums.append(9)
+
+print(nums)
+print(nums.reversed())
+print(nums.sorted())
+
+let details: [String:Any] = [
+    "name": "Yeezy",
+    "Age": 25,
+    "Country": "Canada",
+    "City": "Regina",
+    "Male": true
+]
+
+print(details["name", default: "Unknown"])
+print(details["Age", default: -1])
+print(details["City", default: "Unknown City"])
+
+enum Modes {
+    case jump, lift, sprint
+}
+
+enum Lifts {
+    case squat, bench, deadlift, clean
+}
+
+let supportedLifts: Set<Lifts> = [.squat, .deadlift]
+
+// Checkpoint 2
+
+let albums: [String] = [
+    "Nothing was the same",
+    "If you're reading this it's too late",
+    "Take Care",
+    "Nothing was the same",
+    "If you're reading this it's too late",
+    "Take Care",
+    "Certified Lover Boy",
+    "Certified Lover Boy",
+    "Take Care"]
+
+print("The number of albums is \(albums.count)")
+print("The number of unique albums is \(Set(albums).count)")
