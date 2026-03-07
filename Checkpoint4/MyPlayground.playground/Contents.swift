c4f1a7d3ce59058d74d97a95329bed49748e672b
Eriyeza Buwembo <buwembo.eriyeza@gmail.com>
2026-03-06 19:06:41 -0600

    complete day 8 and checkpoint 4

--- Checkpoint4/MyPlayground.playground/Contents.swift
+++ Checkpoint4/MyPlayground.playground/Contents.swift
@@ -19,9 +19,13 @@ func calculateBarSpeed(distance: Int, startFrame: Int, endFrame: Int, frameRate:
 
 print(calculateBarSpeed(distance: 100, startFrame: 0, endFrame: 87, frameRate: 30))
 
-func range(_ from: Int, _ to: Int) -> [Int] {
+func range(_ from: Int = 0, to: Int) -> [Int] {
     if to < from {
         print("from must be smaller than to")
+        return []
+    }
+    if from < 0 {
+        print("from must be smaller than 0")
         return []
     }
     var res: [Int] = [Int]()
@@ -31,8 +35,48 @@ func range(_ from: Int, _ to: Int) -> [Int] {
     return res
 }
 
-print(range(0,10))
-print(range(0,-1))
+print(range(0,to:10))
+print(range(0,to:-1))
+print(range(to: 10))
+print(range(0,to:-1))
+
+enum IntegerSquareRootError: Error {
+    case valueTooLarge
+    case valueTooSmall
+    case noRoot
+}
+
+func integerSquareRoot(_ n: Int) throws -> Int {
+    if n < 0 {
+        throw IntegerSquareRootError.valueTooSmall
+    } else if n > 10_000 {
+        throw IntegerSquareRootError.valueTooLarge
+    }
+    else {
+        var ans = 0
+        for ans in 0...100 {
+            if ans * ans == n {
+                return ans
+            }
+        }
+        throw IntegerSquareRootError.noRoot
+    }
+}
+
+do {
+    var val = try integerSquareRoot(6400)
+    print(val)
+} catch IntegerSquareRootError.valueTooLarge {
+    print("The value is too large")
+} catch IntegerSquareRootError.valueTooSmall {
+    print("The value is too small")
+} catch IntegerSquareRootError.noRoot {
+    print("There was no integer root found for this number")
+} catch {
+    print("An unknown error occurred")
+}
+
+
 
 
 
